fcf7f5d4b335e333fb6b16903f03c42505ac867c
Sandra Kambo <swairimu2002@gmail.com>
2020-11-13 11:03:54 +0300

    Update gitignore and db volume path

--- src/main/kotlin/io/skambo/example/infrastructure/persistence/jpa/entities/UserDataModel.kt
+++ src/main/kotlin/io/skambo/example/infrastructure/persistence/jpa/entities/UserDataModel.kt
@@ -4,22 +4,22 @@ import javax.persistence.*
 
 @Entity(name = "user")
 data class UserDataModel (
-        @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
-        @Column(nullable = false, length = 256)
+    @Column(nullable = false, length = 256)
     val name: String,
 
-        @Column(nullable = false)
+    @Column(nullable = false)
     val dateOfBirth: String,
 
-        @Column(nullable = false, length = 256)
+    @Column(nullable = false, length = 256)
     val city: String,
 
-        @Column(nullable = false, length = 256)
+    @Column(nullable = false, length = 100, unique = true)
     val email: String,
 
-        @Column(nullable = false, length = 256)
+    @Column(nullable = false, length = 100, unique = true)
     val phoneNumber: String
 )
