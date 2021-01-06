4778c140416e495058bac71d8e6f811a0a546955
Sandra Kambo <swairimu2002@gmail.com>
2021-01-06 15:55:35 +0300

    Add fetch users Swagger definition and refactor

--- src/main/kotlin/io/skambo/example/infrastructure/api/fetchusers/v1/FetchUsersController.kt
+++ src/main/kotlin/io/skambo/example/infrastructure/api/fetchusers/v1/FetchUsersController.kt
@@ -3,7 +3,11 @@ package io.skambo.example.infrastructure.api.fetchusers.v1
 import io.skambo.example.application.domain.model.User
 import io.skambo.example.application.helpers.SortingAndPaginationHelper
 import io.skambo.example.application.services.UserService
+import io.skambo.example.infrastructure.api.common.ApiHeaderKey
+import io.skambo.example.infrastructure.api.common.dto.v1.Header
 import io.skambo.example.infrastructure.api.common.dto.v1.UserDTO
+import io.skambo.example.infrastructure.api.common.helpers.ApiResponseHelper
+import io.skambo.example.infrastructure.api.fetchuser.v1.dto.FetchUserResponse
 import io.skambo.example.infrastructure.api.fetchusers.v1.dto.FetchUsersResponse
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.OffsetDateTime
+import javax.servlet.http.HttpServletRequest
 
 
 @RestController(value = "FetchUsersControllerV1")
@@ -26,7 +31,8 @@ class FetchUsersController(private val userService: UserService) {
         @RequestParam(value = "sortingDirection", defaultValue = "asc") sortingDirection: String,
         @RequestParam(value = "pageSize", defaultValue = "25") pageSize: String,
         @RequestParam(value = "pageNumber", defaultValue = "1") pageNumber: String,
-        @RequestParam(value = "filters", defaultValue = "") filters: String
+        @RequestParam(value = "filters", defaultValue = "") filters: String,
+        httpRequest: HttpServletRequest
     ): ResponseEntity<FetchUsersResponse>{
 
         // Checks that the page number starts at 0
@@ -35,7 +41,14 @@ class FetchUsersController(private val userService: UserService) {
             page, pageSize.toInt(), sortingDirection, orderBy.split(",")
         )
         val usersPage: Page<User> = userService.findUsers(pageRequest, filters)
+
+        val header: Header = Header(
+            messageId = httpRequest.getHeader(ApiHeaderKey.MESSAGE_ID.value),
+            timestamp = OffsetDateTime.parse(httpRequest.getHeader(ApiHeaderKey.TIMESTAMP.value))
+        )
+
         val response: FetchUsersResponse = FetchUsersResponse(
+            header = ApiResponseHelper.createSuccessHeader(httpRequest, header),
             page = page + 1,
             totalPages = usersPage.totalPages,
             numberOfUsers = usersPage.numberOfElements,
@@ -47,8 +60,11 @@ class FetchUsersController(private val userService: UserService) {
                     city = user.city,
                     email = user.email,
                     phoneNumber = user.phoneNumber
-                )}.toList()
+                )}.toList().toTypedArray()
         )
-        return ResponseEntity<FetchUsersResponse>(response, HttpStatus.OK)
+        return ApiResponseHelper.createResponseEntity<FetchUsersResponse>(
+            responseHeader = response.header,
+            body = response
+        )
     }
 }
