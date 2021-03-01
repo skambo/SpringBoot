1c210745052e824e5f16b745cc8f9dd9069b27e4
Sandra Kambo <swairimu2002@gmail.com>
2021-03-01 15:49:12 +0300

    Add request received and response received loggers for createUserController

--- src/main/kotlin/io/skambo/example/infrastructure/api/createuser/v1/CreateUserController.kt
+++ src/main/kotlin/io/skambo/example/infrastructure/api/createuser/v1/CreateUserController.kt
@@ -1,5 +1,6 @@
 package io.skambo.example.infrastructure.api.createuser.v1
 
+import io.skambo.example.MigrationApplication
 import io.skambo.example.application.domain.exceptions.DuplicateUserException
 import io.skambo.example.application.domain.model.User
 import io.skambo.example.application.services.UserService
@@ -7,6 +8,7 @@ import io.skambo.example.infrastructure.api.common.helpers.ApiResponseHelper
 import io.skambo.example.infrastructure.api.createuser.v1.dto.CreateUserRequest
 import io.skambo.example.infrastructure.api.createuser.v1.dto.CreateUserResponse
 import io.skambo.example.infrastructure.api.fetchuser.v1.dto.FetchUserResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,12 +20,14 @@ import javax.servlet.http.HttpServletRequest
 @RestController(value = "CreateUserControllerV1")
 @RequestMapping(value = ["v1/"])
 class CreateUserController(private val userService: UserService){
+    private val LOGGER = LoggerFactory.getLogger(CreateUserController::class.java)
 
     @PostMapping(value = ["createUser"])
     fun createUser(
         @RequestBody createUserRequest: CreateUserRequest,
         httpRequest: HttpServletRequest
     ): ResponseEntity<CreateUserResponse> {
+        LOGGER.info("Request received: $createUserRequest")
         val user: User = User (
             name = createUserRequest.name,
             dateOfBirth = createUserRequest.dateOfBirth,
@@ -40,10 +44,12 @@ class CreateUserController(private val userService: UserService){
             email = createdUser.email,
             city = createdUser.city,
             phoneNumber = createdUser.phoneNumber)
-        return ApiResponseHelper.createResponseEntity<CreateUserResponse>(
+        val responseEntity: ResponseEntity<CreateUserResponse> = ApiResponseHelper.createResponseEntity<CreateUserResponse>(
             responseHeader = response.header,
             body = response,
             httpStatusCode = HttpStatus.CREATED
         )
+        LOGGER.info("Response returned: $responseEntity")
+        return responseEntity
     }
 }
